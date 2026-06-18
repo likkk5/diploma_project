9874ef86589f235062cf6e6373419d8079359cb9
lishykksu <lishykksu@gmail.com>
2026-06-18 19:58:46 +0300

    add variables to env

--- myProject/myProject/settings.py
+++ myProject/myProject/settings.py
@@ -26,10 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!3_852&*9)cjq_!@a##jw=2)grg@ijj1pegx0ut!8*8nbf1cf@'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1', '.koyeb.app']
 
